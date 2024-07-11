1e4b7b8d01f700403ad19e6fd081e78027e62cd9
Abdelfattah Elnaggar <elnaggara877@gmail.com>
2024-07-11 17:23:02 +0300

    The sixth day-JS

---
+++ day06_JS/master.js
@@ -0,0 +1,47 @@
+function showText() {
+  let colorValue = document.getElementById("color").value;
+  let textValue = document.getElementById("text").value;
+
+  console.log(colorValue);
+
+  if (colorValue != "") {
+    document.getElementById("show").innerHTML = textValue;
+    document.getElementById("show").style.display = "block";
+    document.getElementById("show").style.color = "#fff";
+    document.getElementById("show").style.backgroundColor = colorValue;
+  } else if (textValue != "" && colorValue == "") {
+    document.getElementById("show").innerHTML = textValue;
+    document.getElementById("show").style.color = "black";
+    document.getElementById("show").style.display = "block";
+    document.getElementById("show").style.backgroundColor = colorValue;
+  } else if (textValue == "" && colorValue == "") {
+    window.alert("At least enter one value");
+  }
+}
+
+// =============================================
+
+function addOper() {
+  let firstNum = Number(document.getElementById("number1").value);
+  let secondNum = Number(document.getElementById("number2").value);
+  console.log(firstNum);
+
+  if (firstNum == "" || secondNum == "") {
+    window.alert("Ensure that you enter number to each field");
+  } else {
+    var addResult = `${firstNum} + ${secondNum} = ${firstNum + secondNum}`;
+    document.getElementById("addResult").innerHTML = addResult;
+  }
+}
+function multOper() {
+  let firstNum = Number(document.getElementById("number1").value);
+  let secondNum = Number(document.getElementById("number2").value);
+  console.log(firstNum);
+
+  if (firstNum == "" || secondNum == "") {
+    window.alert("Ensure that you enter number to each field");
+  } else {
+    var addResult = `${firstNum} x ${secondNum} = ${firstNum * secondNum}`;
+    document.getElementById("multResult").innerHTML = addResult;
+  }
+}
