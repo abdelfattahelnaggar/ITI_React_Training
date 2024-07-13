cb28658d4835b18b7acb451a37c3c8539e4ce908
Abdelfattah Elnaggar <elnaggara877@gmail.com>
2024-07-13 15:25:15 +0300

    fix problems

--- day06_JS/master.js
+++ day06_JS/master.js
@@ -4,44 +4,45 @@ function showText() {
 
   console.log(colorValue);
 
-  if (colorValue != "") {
+  if (colorValue !== "") {
     document.getElementById("show").innerHTML = textValue;
     document.getElementById("show").style.display = "block";
     document.getElementById("show").style.color = "#fff";
     document.getElementById("show").style.backgroundColor = colorValue;
-  } else if (textValue != "" && colorValue == "") {
+  } else if (textValue !== "" && colorValue === "") {
     document.getElementById("show").innerHTML = textValue;
     document.getElementById("show").style.color = "black";
     document.getElementById("show").style.display = "block";
-    document.getElementById("show").style.backgroundColor = colorValue;
-  } else if (textValue == "" && colorValue == "") {
+    document.getElementById("show").style.backgroundColor = "transparent";
+  } else if (textValue === "" && colorValue === "") {
     window.alert("At least enter one value");
   }
+  
 }
-
-// =============================================
 
 function addOper() {
   let firstNum = Number(document.getElementById("number1").value);
   let secondNum = Number(document.getElementById("number2").value);
   console.log(firstNum);
 
-  if (firstNum == "" || secondNum == "") {
-    window.alert("Ensure that you enter number to each field");
+  if (isNaN(firstNum) || isNaN(secondNum)) {
+    window.alert("Ensure that you enter a number in each field");
   } else {
     var addResult = `${firstNum} + ${secondNum} = ${firstNum + secondNum}`;
     document.getElementById("addResult").innerHTML = addResult;
   }
 }
+
 function multOper() {
   let firstNum = Number(document.getElementById("number1").value);
   let secondNum = Number(document.getElementById("number2").value);
   console.log(firstNum);
 
-  if (firstNum == "" || secondNum == "") {
-    window.alert("Ensure that you enter number to each field");
+  if (isNaN(firstNum) || isNaN(secondNum)) {
+    window.alert("Ensure that you enter a number in each field");
   } else {
-    var addResult = `${firstNum} x ${secondNum} = ${firstNum * secondNum}`;
-    document.getElementById("multResult").innerHTML = addResult;
+    var multResult = `${firstNum} x ${secondNum} = ${firstNum * secondNum}`;
+    document.getElementById("multResult").innerHTML = multResult;
   }
 }
+
